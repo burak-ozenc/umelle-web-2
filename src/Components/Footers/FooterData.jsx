a0ab9c154a5e93b7944dc2a1322523b30b7b96f0
Ivan_N <admin@umelle.com>
2024-03-20 15:05:26 +0200

    fixes

--- src/Components/Footers/FooterData.jsx
+++ src/Components/Footers/FooterData.jsx
@@ -1,152 +1,124 @@
 const FooterData = [
     {
-        title: "company",
-        submenu: [
-            {
-                title: "About company",
-                link: "/page/about-us"
-            },
-            {
-                title: "Company services",
-                link: "/page/our-services"
-            },
-            {
-                title: "Job opportunities",
-                link: "/page/our-team"
-            },
-            {
-                title: "Contact us",
-                link: "/page/contact-classic"
-            },
-        ]
-    },
-    {
-        title: "customer",
-        submenu: [
-            {
-                title: "Client support",
-                link: "/page/faq-s"
-            },
-            {
-                title: "Pricing packages",
-                link: "/page/pricing-packages"
-            },
-            {
-                title: "Company history",
-                link: "/page/our-story"
-            },
-            {
-                title: "Our process",
-                link: "/page/our-process"
-            },
-        ]
-    },
-    {
-        title: "Additional",
-        submenu: [
-            {
-                title: "Our story",
-                link: "/page/our-story"
-            },
-            {
-                title: "Who we are",
-                link: "/page/who-we-are"
-            },
-            {
-                title: "Our process",
-                link: "/page/our-process"
-            },
-            {
-                title: "Latest news",
-                link: "/page/latest-news"
-            },
-        ]
-    },
-    {
         title: "Services",
         submenu: [
             {
-
-                title: "Brand experience",
-                link: "/page/our-services"
-            },
-            {
-                title: "e-Commerce website",
-                link: "/page/our-services"
-            },
-            {
-                title: "Content writing",
-                link: "/page/our-services"
-            },
-            {
-                title: "Marketing strategy",
-                link: "/page/our-services"
-            },
-        ]
-    },
-    {
-        title: "Resources",
-        submenu: [
-            {
-                title: "Theme guide",
-                link: "/page/faq-s"
-            },
-            {
-                title: "Support desk",
-                link: "/page/faq-s"
-            },
-            {
-                title: "What we offer",
-                link: "/page/what-we-offer"
-            },
-            {
-                title: "Company history",
-                link: "/page/our-story"
+                title: 'Web & Desktop',
+                link: '/services#webDesktop'
+            }, {
+                title: 'Mobile',
+                link: '/services#crossPlatformAndMobile'
+            }, {
+                title: 'Data Integration and Formats',
+                link: '/services#dataIntegrationAndFormats'
+            }, {
+                title: 'Content Management and Web APIs',
+                link: '/services#contentManagement'
+            }, {
+                title: 'Cloud Services and Deployment',
+                link: '/services#cloudServices'
+            }, {
+                title: 'UX/UI Design',
+                link: '/services#uxUI'
             },
         ]
     },
     {
-        title: "Categories",
+        title: "Solutions",
         submenu: [
             {
-                title: "For men",
-                link: "/shop/shop-wide"
+                title: "Insurance Management",
+                link: "/solutions"
             },
             {
-                title: "For woman",
-                link: "/shop/shop-wide"
+                title: "Operations Management",
+                link: "/solutions"
             },
+            // {
+            //     title: "Job opportunities",
+            //     link: "/page/our-team"
+            // },
+            // {
+            //     title: "Contact us",
+            //     link: "/page/contact-classic"
+            // },
+        ]
+    },
+    {
+        title: "Products",
+        submenu: [
             {
-                title: "Accessories",
-                link: "/shop/shop-wide"
-            },
-            {
-                title: "Collections",
-                link: "/shop/shop-wide"
+                title: 'Umelle Insurance Management Suite UI MS ',
+                link: '/coming-soon'
             },
         ]
     },
     {
-        title: "Adavantages",
+        title: "Company",
         submenu: [
             {
-                title: "Free delivery",
-                link: "/page/faq-s"
+                title: 'About UMELLE',
+                link: '/company'
             },
             {
-                title: "100 days refund",
-                link: "/page/faq-s"
-            },
-            {
-                title: "Multiple payments",
-                link: "/page/faq-s"
-            },
-            {
-                title: "Sustainable",
-                link: "/page/faq-s"
+                title: 'Blog',
+                link: '/coming-soon'
             },
         ]
     },
+    {
+        title: "Products",
+        submenu: [
+            {
+                title: 'Umelle Insurance Management Suite UI MS ',
+                link: '/coming-soon'
+            },
+        ]
+    },
+    
+    // {
+    //     title: "Additional",
+    //     submenu: [
+    //         {
+    //             title: "Our story",
+    //             link: "/page/our-story"
+    //         },
+    //         {
+    //             title: "Who we are",
+    //             link: "/page/who-we-are"
+    //         },
+    //         {
+    //             title: "Our process",
+    //             link: "/page/our-process"
+    //         },
+    //         {
+    //             title: "Latest news",
+    //             link: "/page/latest-news"
+    //         },
+    //     ]
+    // },
+    
+    // {
+    //     title: "Adavantages",
+    //     submenu: [
+    //         {
+    //             title: "Free delivery",
+    //             link: "/page/faq-s"
+    //         },
+    //         {
+    //             title: "100 days refund",
+    //             link: "/page/faq-s"
+    //         },
+    //         {
+    //             title: "Multiple payments",
+    //             link: "/page/faq-s"
+    //         },
+    //         {
+    //             title: "Sustainable",
+    //             link: "/page/faq-s"
+    //         },
+    //     ]
+    // },
 ]
 
 export default FooterData
